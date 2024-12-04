be1293d69d1ce529861b4d19efb23676ca00306e
William Blackerby <wmblackerby@gmail.com>
2024-12-04 15:46:46 -0500

    checkpoint

--- src/constants.rs
+++ src/constants.rs
@@ -5,8 +5,4 @@ pub const G0_SET: [u8; 3] = [b'(', b',', b'$'];
 pub const G1_SET: [u8; 3] = [b')', b'-', b'$'];
 pub const BLANK: char = ' ';
 pub const MULTI_BYTE: u8 = 0x31;
-const ESCAPE: u8 = 0x1b;
-
-pub const fn is_escape(c: u8) -> bool {
-    c == ESCAPE
-}
+pub const ESCAPE: u8 = 0x1b;
