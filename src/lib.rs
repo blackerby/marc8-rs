766d37a1d92627d8f9323f6ca863c984527b5a5b
William Blackerby <wmblackerby@gmail.com>
2024-12-05 20:42:45 -0500

    move constants to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,27 @@
 mod charsets;
-mod constants;
 mod error;
 
 use std::borrow::Cow;
 use std::char;
 use unicode_normalization::UnicodeNormalization;
 
-use crate::constants::*;
 use crate::error::EncodingError;
 #[cfg(feature = "python")]
 use pyo3::prelude::*;
 #[cfg(feature = "python")]
 use pyo3::types::{PyBytes, PyString};
+
+pub const BASIC_LATIN: u8 = 0x42;
+pub const REDESIGNATED_ASCII: u8 = 0x73;
+pub const ANSEL: u8 = 0x45;
+pub const G0_SET: [u8; 3] = [b'(', b',', b'$'];
+pub const G1_SET: [u8; 3] = [b')', b'-', b'$'];
+pub const BLANK: char = ' ';
+pub const MULTI_BYTE: u8 = 0x31;
+pub const ESCAPE: u8 = 0x1b;
+pub const CODESETS: [u8; 12] = [
+    0x31, 0x32, 0x33, 0x34, 0x42, 0x45, 0x4E, 0x51, 0x53, 0x62, 0x67, 0x70,
+];
 
 pub struct Decoder {
     g0: u8,
