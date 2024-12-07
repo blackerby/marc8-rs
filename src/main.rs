cdff3c66407713b99ac60f9f6b25251eae4ba22d
William Blackerby <wmblackerby@gmail.com>
2024-12-07 13:44:25 -0500

    read from stdin and add flag for quiet mode

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,34 @@
 #![cfg(feature = "cli")]
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{self, BufRead, BufReader};
 
 use clap::Parser;
 use marc8::Decoder;
 
 #[derive(Parser)]
 struct Config {
+    /// Path to a file in MARC-8 encoding.
     #[arg(default_value = "-")]
     path: Option<String>,
+    /// Hide UTF-8 warnings. Default: false.
+    #[arg(short, long, default_value_t = false)]
+    quiet: bool,
 }
-// TODO: add ability to read from stdin
 // TODO: test output
-// TODO: add flag for quiet option
+// TODO: improve error handling
 fn main() {
     let config = Config::parse();
+    let mut decoder = Decoder::new(None, None, Some(config.quiet));
 
-    let file = File::open(config.path.unwrap()).unwrap();
-    let reader = BufReader::new(file);
-    let mut decoder = Decoder::new(None, None, None);
-
-    for line in reader.lines() {
-        let bytes = line.unwrap().into_bytes();
-        let result = decoder.decode(&bytes).unwrap();
-        println!("{}", result);
+    if let Some(ref path) = config.path {
+        let reader: Box<dyn BufRead> = match path.as_str() {
+            "-" => Box::new(BufReader::new(io::stdin())),
+            _ => Box::new(BufReader::new(File::open(path).unwrap())),
+        };
+        for line in reader.lines() {
+            let bytes = line.unwrap().into_bytes();
+            let result = decoder.decode(&bytes).unwrap();
+            println!("{}", result);
+        }
     }
 }
