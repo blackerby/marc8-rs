b70ee02ee936c57c7ab1692b091247d52ea62369
William Blackerby <wmblackerby@gmail.com>
2024-12-06 22:58:58 -0500

    read from file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
+#![cfg(feature = "cli")]
+use std::fs::File;
+use std::io::Read;
+
+use clap::Parser;
 use marc8::Decoder;
 
+#[derive(Parser)]
+struct Config {
+    #[arg(default_value = "-")]
+    path: Option<String>,
+}
+// TODO: add ability to read from stdin
+// TODO: test output
 fn main() {
-    println!("Hello, world!");
+    let config = Config::parse();
+
+    let mut reader = File::open(config.path.unwrap()).unwrap();
+    let mut buffer = Vec::new();
+    reader.read_to_end(&mut buffer).unwrap();
+    let mut decoder = Decoder::new(None, None, None);
+    let result = decoder.decode(&buffer).unwrap();
+
+    println!("{result}");
 }
