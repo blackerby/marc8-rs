4427f4384cbfbff39b9cc087920655fe7a7dbe31
William Blackerby <wmblackerby@gmail.com>
2024-12-08 19:58:23 -0500

    cleanup

--- marc8-cli/src/main.rs
+++ marc8-cli/src/main.rs
@@ -4,8 +4,11 @@ use std::io::{self, BufRead, BufReader};
 use clap::Parser;
 use marc8::Marc8;
 
+// TODO: change path type to PathBuf?
+// TODO: add arg for g0
+// TODO: add arg for g1
 #[derive(Parser)]
-struct Config {
+struct Cli {
     /// Path to a file in MARC-8 encoding.
     #[arg(default_value = "-")]
     path: Option<String>,
@@ -13,13 +16,14 @@ struct Config {
     #[arg(short, long, default_value_t = false)]
     quiet: bool,
 }
-// TODO: test output
+// TODO: test reading from stdin
+// TODO: test reading from file
 // TODO: improve error handling
 fn main() {
-    let config = Config::parse();
-    let mut converter = Marc8::new(None, None, Some(config.quiet));
+    let cli = Cli::parse();
+    let mut converter = Marc8::new(None, None, Some(cli.quiet));
 
-    if let Some(ref path) = config.path {
+    if let Some(ref path) = cli.path {
         let reader: Box<dyn BufRead> = match path.as_str() {
             "-" => Box::new(BufReader::new(io::stdin())),
             _ => Box::new(BufReader::new(File::open(path).unwrap())),
